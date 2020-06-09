c5efee6fe34138b24c347fa1b0695a7c8adf9252
rezafahlavi <rezafahlavi73@gmail.com>
2020-06-10 04:11:24 +0700

    update sql

--- inventori.sql
+++ inventori.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Bulan Mei 2020 pada 11.08
--- Versi server: 10.1.30-MariaDB
--- Versi PHP: 7.2.2
+-- Generation Time: Jun 09, 2020 at 11:07 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,87 +24,155 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bahan`
+-- Table structure for table `bahan`
 --
 
 CREATE TABLE `bahan` (
-  `id_bahan` varchar(20) NOT NULL,
+  `id_bahan` int(20) NOT NULL,
   `nama_bahan` varchar(20) NOT NULL,
   `ukuran` varchar(20) NOT NULL,
   `berat` int(20) NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `bahan`
+--
+
+INSERT INTO `bahan` (`id_bahan`, `nama_bahan`, `ukuran`, `berat`, `jumlah`) VALUES
+(1, 'Wire rod bwg 6', 'Bwg 6', 50, 105),
+(2, 'Wire rod bwg 8', 'Bwg 8', 50, 200),
+(3, 'Wire rod bwg 10', 'Bwg 10', 50, 200),
+(4, 'Wire rod bwg 12', 'Bwg 12', 50, 300),
+(5, 'Wire rod bwg 6', 'Bwg 6', 100, 100),
+(6, 'Wire rod bwg 8', 'Bwg 8', 100, 60),
+(7, 'Wire rod bwg 10', 'Bwg 10', 100, 80),
+(8, 'Wire rod bwg 12', 'Bwg 12', 100, 100);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bahan_keluar`
+-- Table structure for table `bahan_keluar`
 --
 
 CREATE TABLE `bahan_keluar` (
-  `id_bahan_keluar` varchar(20) NOT NULL,
+  `id_bahan_keluar` int(20) NOT NULL,
   `id_bahan` varchar(20) NOT NULL,
+  `nama_bahan` varchar(20) NOT NULL,
+  `berat` varchar(20) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
   `tgl_keluar` date NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `bahan_keluar`
+--
+
+INSERT INTO `bahan_keluar` (`id_bahan_keluar`, `id_bahan`, `nama_bahan`, `berat`, `ukuran`, `tgl_keluar`, `jumlah`) VALUES
+(1, '1', 'Wire rod bwg 6', '50', 'Bwg 6', '2020-06-02', 5);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bahan_masuk`
+-- Table structure for table `bahan_masuk`
 --
 
 CREATE TABLE `bahan_masuk` (
-  `id_bahan_masuk` varchar(20) NOT NULL,
+  `id_bahan_masuk` int(20) NOT NULL,
   `id_bahan` varchar(20) NOT NULL,
+  `nama_bahan` varchar(20) NOT NULL,
+  `berat` varchar(20) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
   `tgl_masuk` date NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `bahan_masuk`
+--
+
+INSERT INTO `bahan_masuk` (`id_bahan_masuk`, `id_bahan`, `nama_bahan`, `berat`, `ukuran`, `tgl_masuk`, `jumlah`) VALUES
+(1, '1', 'Wire rod bwg 6', '50', 'Bwg 6', '2020-06-01', 10);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
-  `id_barang` varchar(20) NOT NULL,
+  `id_barang` int(20) NOT NULL,
   `nama_barang` varchar(30) NOT NULL,
   `ukuran` varchar(20) NOT NULL,
   `berat` int(20) NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `ukuran`, `berat`, `jumlah`) VALUES
+(1, 'Paku 2 inch', '2x12', 30, 200),
+(2, 'Paku 3 inch', '3x10', 30, 200),
+(3, 'Paku 4 Inch', '4x8', 30, 400),
+(4, 'Paku 5 inch', '5x6', 30, 300),
+(5, 'Paku 2 inch', '2x12', 10, 200),
+(6, 'Paku 3 inch', '3x10', 10, 650),
+(7, 'Paku 4 inch', '4x8', 10, 300),
+(8, 'Paku 5 inch', '5x6', 10, 300);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_keluar`
+-- Table structure for table `barang_keluar`
 --
 
 CREATE TABLE `barang_keluar` (
-  `id_barang_keluar` varchar(20) NOT NULL,
+  `id_barang_keluar` int(20) NOT NULL,
   `id_barang` varchar(20) NOT NULL,
+  `nama_barang` varchar(20) NOT NULL,
+  `berat` varchar(20) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
   `tgl_keluar` date NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`id_barang_keluar`, `id_barang`, `nama_barang`, `berat`, `ukuran`, `tgl_keluar`, `jumlah`) VALUES
+(1, '1', 'Paku 2 inch', '30', '2x12', '2020-06-02', 50);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_masuk`
+-- Table structure for table `barang_masuk`
 --
 
 CREATE TABLE `barang_masuk` (
-  `id_barang_masuk` varchar(20) NOT NULL,
+  `id_barang_masuk` int(20) NOT NULL,
   `id_barang` varchar(20) NOT NULL,
+  `nama_barang` varchar(20) NOT NULL,
+  `berat` varchar(20) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
   `tgl_masuk` date NOT NULL,
   `jumlah` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_barang_masuk`, `id_barang`, `nama_barang`, `berat`, `ukuran`, `tgl_masuk`, `jumlah`) VALUES
+(7, '6', 'Paku 3 inch', '10', '3x10', '2020-06-01', 500);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `penjualan`
+-- Table structure for table `penjualan`
 --
 
 CREATE TABLE `penjualan` (
@@ -121,66 +188,119 @@ CREATE TABLE `penjualan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `id_user` varchar(20) NOT NULL,
+  `id_user` int(20) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
+(2, 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `bahan`
+-- Indexes for table `bahan`
 --
 ALTER TABLE `bahan`
   ADD PRIMARY KEY (`id_bahan`);
 
 --
--- Indeks untuk tabel `bahan_keluar`
+-- Indexes for table `bahan_keluar`
 --
 ALTER TABLE `bahan_keluar`
   ADD PRIMARY KEY (`id_bahan_keluar`);
 
 --
--- Indeks untuk tabel `bahan_masuk`
+-- Indexes for table `bahan_masuk`
 --
 ALTER TABLE `bahan_masuk`
   ADD PRIMARY KEY (`id_bahan_masuk`);
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indeks untuk tabel `barang_keluar`
+-- Indexes for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
   ADD PRIMARY KEY (`id_barang_keluar`);
 
 --
--- Indeks untuk tabel `barang_masuk`
+-- Indexes for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
   ADD PRIMARY KEY (`id_barang_masuk`);
 
 --
--- Indeks untuk tabel `penjualan`
+-- Indexes for table `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD PRIMARY KEY (`id_penjualan`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bahan`
+--
+ALTER TABLE `bahan`
+  MODIFY `id_bahan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `bahan_keluar`
+--
+ALTER TABLE `bahan_keluar`
+  MODIFY `id_bahan_keluar` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bahan_masuk`
+--
+ALTER TABLE `bahan_masuk`
+  MODIFY `id_bahan_masuk` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `id_barang_keluar` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_barang_masuk` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
