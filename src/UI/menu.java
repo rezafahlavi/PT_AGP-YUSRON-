9a7cd78c95a2f0717dfd08b62447a2f369ffb0c6
rezafahlavi <rezafahlavi73@gmail.com>
2020-07-19 15:05:45 +0700

    menu edit

--- src/UI/menu.java
+++ src/UI/menu.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI;
 
-/**
- *
- * @author Aditya Putra Pratama
- */
 public class menu extends javax.swing.JFrame {
 
-    /**
-     * Creates new form menu
-     */
+
     public menu() {
         initComponents();
     }
